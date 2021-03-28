d61f745746657588a311a973b96fa358c425739a
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-03-28 15:37:38 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
 # =======
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
